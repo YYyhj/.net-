0a7a77fda775cbdfabcb34446190d994d1360909
YYyhj <39551889+YYyhj@users.noreply.github.com>
2019-01-08 10:46:14 +0800

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" Dubug="true" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="News._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>无标题页</title>
+</head>
+<body>
+     <form id="form1" runat="server">
+    <div>
+    
+        新闻标题：<asp:TextBox runat="server" ID="Text1" /></div>
+    <p>
+        作者： 
+        <asp:TextBox runat="server" ID="Text2" /></p>
+    <p>
+                内容：
+        <asp:TextBox runat="server" ID="Text3" /></p>
+    <p>
+        发布时间：<asp:TextBox runat="server" ID="time" /></p></form>
+<p>
+   <asp:DropDownList ID="Select1" runat="server" AppendDataBoundItems="True" 
+                    DataSourceID="News" DataTextField="CategoryName" DataValueField="CategoryId">
+                    <asp:ListItem disabled Selected>请选择新闻类型</asp:ListItem>
+                </asp:DropDownList>
+                <asp:SqlDataSource ID="Select" runat="server" 
+                    ConnectionString="<%$ ConnectionStrings:ConnectionString %>" 
+                    SelectCommand="SELECT [CategoryId], [CategoryName] FROM [NewsCategoryName]">
+                </asp:SqlDataSource></p>
+                   
+<input id="Button1" type="button" value="button" />  
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+namespace News
+{
+   private string connString = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ConnectionStringSettings connectionStringSettings =
+               ConfigurationManager.ConnectionStrings["ConnectionString"];
+            if (connectionStringSettings == null ||
+                string.IsNullOrEmpty(connectionStringSettings.ConnectionString))
+                throw new ApplicationException("未找到数据库连接字符串。");
+            this.connString = connectionStringSettings.ConnectionString;
+            if (IsPostBack)
+            {
+                lbMessage.Visible = true;
+            }
+            else
+            {
+                lbMessage.Visible = false;
+            }
+        }
+
+        protected void on_button(object sender, EventArgs e)
+        {
+            string sql = 
+                //"Data Source=.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\News.mdf;Integrated Security=True;User Instance=True";
+            //string sql =
+                "INSERT INTO News(Title, CategoryId, Content, AddTime) " ;
+               "VALUES(@Title, @Author, @CategoryId, @Content, @AddTime)";
+      using (SqlConnection conn = new SqlConnection(connString))
+      {
+          using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+            //SqlConnection conn = new SqlConnection(connStr);
+          //  SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@Title",Text1.Text);
+            cmd.Parameters.AddWithValue("@Author",Text2.Text);                                                                                                                                                              
+            cmd.Parameters.AddWithValue("@Content",Text3.Text);
+            cmd.Parameters.AddWithValue("@AddTime",time.Text);
+            cmd.Parameters.AddWithValue("@CategoryId",Select1.SelectedValue);
+           try
+                    {
+                        conn.Open();
+                        if (cmd.ExecuteNonQuery() == 1)
+                        {
+                            lbMessage.Text = "添加学生信息成功。";
+                            lbMessage.Style.Add("color", "green");
+                            tbForm.Visible = false;
+                        }
+                        else
+                        {
+                            lbMessage.Text = "添加失败。";
+                            lbMessage.Style.Add("color", "red");
+                        }
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        lbMessage.Text = "添加失败。" + ex.Message;
+                        lbMessage.Style.Add("color", "red");
+                    }
+                }
+      }
+        
+        }
+    }
+}
+}
